c668f3675de912a31e148bcbd9fb378a00f1bad5
ttken01 <70005383+ttken01@users.noreply.github.com>
2020-09-12 21:39:41 +0700

    ADD SLIDEBAR
    thêm vào slide bar khi onclick vào mua hàng

--- JS/script.js
+++ JS/script.js
@@ -12,6 +12,7 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
 }
+
 //GO TO TOP
 $(document).ready(function(){
     $(window).scroll(function(){
@@ -20,6 +21,26 @@ $(document).ready(function(){
             $("#Gotop").show('slow');
         else
             $("#Gotop").hide('slow');
+    })
+// lấy ảnh mua hàng vào thanh toán
+    $(".note-page input").click(function(){
+        var price = $(this).attr('data-price');
+        var name = $(this).attr('data-name');
+        var c = $(this).attr('data');
+        $("#imgSlide").attr("src", `IMG/${c}_1.jpg`)
+        $("#noteSlide").text(name)
+        $("#priceSlide").text(price)
+        $("#imgSlide1").attr("src", `IMG/${c}_1.jpg`)
+        $("#imgSlide2").attr("src", `IMG/${c}_2.jpg`)
+        $("#imgSlide3").attr("src", `IMG/${c}_3.jpg`)
 
     })
+//thumb nail cho ảnh
+    $(".thumb img").click(function(){
+        $(this).attr('src')
+        $("#imgSlide").attr("src", $(this).attr('src'))
+    })
+
 })
+
+
