a078a5faa909a23faea9dc07af47e97139b97e6e
ttken01 <70005383+ttken01@users.noreply.github.com>
2020-09-12 15:07:49 +0700

    Top And Slide bar
    thêm go to top và slide bar mua hàng

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,25 @@
-$(document).ready(function(){
 
+//hàm tạo background cho nav menu khi lăn chuột cuộn trang 
+window.addEventListener("scroll", function(){
+    var a = document.querySelector("nav.menu")
+    a.classList.toggle("menu-scroll", window.scrollY > 0);
+})
+// hàm mở khung đặt hàng
+function openNav() {
+    document.getElementById("mySidebar").style.width = "400px";
+  }
+//Hàm đóng khung đặt hàng
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+}
+//GO TO TOP
+$(document).ready(function(){
     $(window).scroll(function(){
         var t = $(this).scrollTop()
-        if(t > 10)
-            $("#navmenu").animate({
-                'background-color' : 'none'
-            })
+        if(t > 50)
+            $("#Gotop").show('slow');
         else
-            $("#navmenu").animate({
-                'background-color' : 'rgba(0, 0, 0, 0.4)'
-            })
-    })    
+            $("#Gotop").hide('slow');
+
+    })
 })
